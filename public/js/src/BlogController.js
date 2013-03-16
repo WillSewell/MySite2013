f2833859452a3f1d16970c935e2aec81b9aee28a
Will Sewell <me@willsewell.name>
2013-03-16 13:36:10 +1100

    Removed unused code from the contact controller.

--- public/js/src/BlogController.js
+++ public/js/src/BlogController.js
@@ -1,10 +1,17 @@
 BlogController = {
+    areMorePosts: true,
     init: function() {
         $(window).scroll(function () {
             // if scrolled to the bottom, requests more posts
-            if ($(window).scrollTop() >= $(document).height() - $(window).height() - 10) {
+            if ($(window).scrollTop() >= $(document).height() - $(window).height() - 10
+                && BlogController.areMorePosts
+            ) {
                 $.get('blog/getMorePosts/' + $('#posts div').length, function(posts) {
-                    $('#posts').append(posts);
+                    if (posts) {
+                        $('#posts').append(posts);
+                    } else {
+                        BlogController.areMorePosts = false;
+                    }
                 });
             }
         });
--- public/js/src/SiteController.js
+++ public/js/src/SiteController.js
@@ -3,35 +3,49 @@ SiteController = {
         require.config({
             urlArgs: "bust=" + Math.floor((Math.random()*100)+1) // CACHEBUSTER - REMOVE IN PROD
         });
-        require(['lib/jquery', 'lib/html5', 'lib/less'], function() {
-            require(['lib/pagify', 'site_listeners'], function() {
-                $(window).scroll(sticky_nav_listener);
-                $(window).resize(sticky_nav_listener);
-
+        require(['lib/jquery', 'lib/html5'], function() {
+            require(['lib/pagify'], function() {
+                $(window).scroll(SiteController.sticky_nav_listener);
+                $(window).resize(SiteController.sticky_nav_listener);
                 $('#main').pagify({
                     pages: ['home', 'blog', 'contact'],
                     animation: 'fadeIn',
                     onChange: function(page) {
                         // check the page, and include the required files
                         if (page === 'blog') {
-                            append_stylesheet('../css/styles.css');
+                            SiteController.append_stylesheet('../css/styles.css');
                             require(['BlogController'], function() {
                                 BlogController.init();
                             });
                         } else if (page === 'contact') { 
-                            append_stylesheet('../css/contact.css');
-                            require(['ContactController'], function() {
-                                ContactController.init();
-                            });
+                            SiteController.append_stylesheet('../css/contact.css');
                         }
                     }
                 });
-
-                $('#contact_link').click(function() {
-                    append_stylesheet('../css/contact.css');
-                })
             });
         });
+    },
+    sticky_nav_listener: function() {
+        if ($(window).scrollTop() > 0) {
+            $('#header').addClass('fixed').css({
+                'top'  : '0',
+                'width': $('#page').width()
+            });
+            //$('#main').css('padding-top','3em');
+        } else {
+
+            $('#header').removeClass('fixed');
+            //$('#main').css('padding-top','0em');
+        }
+    },
+    append_stylesheet: function(path) {
+        if ($('head link[href="' + path + '"]').length === 0) { // is it already there?
+            if (document.createStyleSheet) { // IE uses this
+                document.createStyleSheet(path);
+            } else {
+                $('head').append('<link rel="stylesheet" href="' + path + '" type="text/css" />');
+            }
+        }
     }
 };
 
